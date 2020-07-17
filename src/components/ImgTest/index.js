ece1adf9be64fbaa75dd3e86029c4a6610cfa1dd
Sun Mengge <sunmengge@xiaoheiban.cn>
2020-07-17 15:47:45 +0800

    feat: 测试sideEffects

--- src/components/ImgTest/index.js
+++ src/components/ImgTest/index.js
@@ -4,10 +4,11 @@ import img1 from '../../assets/images/8694a4c27d1ed21bcf007ab8b34a4ec250da3f48.p
 import img2 from '../../assets/images/defultHead.svg';
 import './index.less';
 
-function ImgTest() {
-  return (
-    <img src={maoImg} className='mao-img' />
-  )
+export default class ImgTest extends React.Component {
+  render() {
+    return (
+      <img src={maoImg} className='mao-img' />
+    )
+  }
 }
 
-export default React.memo(ImgTest);
