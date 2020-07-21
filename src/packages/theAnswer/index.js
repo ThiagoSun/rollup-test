6be6a73b742cfbc269bb1fbf3dbc57452c56464e
Sun Mengge <sunmengge@xiaoheiban.cn>
2020-07-21 19:17:50 +0800

    feat: 不要写console语句，防止干扰测试

--- src/packages/theAnswer/index.js
+++ src/packages/theAnswer/index.js
@@ -1,24 +1,23 @@
 import answer from 'the-answer';
 
 const f0 = () => {
-  console.log('the answer is ' + answer);
+  return answer;
 }
 
 const f1 = () => {
-  console.log(1);
+  return 1;
 };
 
 const f2 = () => {
-  console.log(2);
+  return 2;
 };
 
 const f3 = () => {
-  console.log(3);
+  return 3;
 }
 
 const f4 = () => {
-  console.log(4);
-  f3();
+  return f3() + 1;
 }
 
 export default {
